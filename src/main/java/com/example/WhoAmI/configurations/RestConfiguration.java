eabcf3b71d126f06ca5f9a6a9851ba1d9cb6edfe
Nigel <nigelmassey90@gmail.com>
2020-07-11 12:58:44 +0100

    Added Timeout settings to the RestTemplate configuration.

--- src/main/java/com/example/WhoAmI/configurations/RestConfiguration.java
+++ src/main/java/com/example/WhoAmI/configurations/RestConfiguration.java
@@ -5,13 +5,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
 
+import java.time.Duration;
+
 @Configuration
 public class RestConfiguration {
 
     @Bean
     public RestTemplate restTemplate(RestTemplateBuilder restTemplateBuilder){
 
-        return restTemplateBuilder.build();
+        return restTemplateBuilder
+                .setConnectTimeout(Duration.ofSeconds(15))
+                .setReadTimeout(Duration.ofSeconds(15))
+                .build();
 
     }
 
