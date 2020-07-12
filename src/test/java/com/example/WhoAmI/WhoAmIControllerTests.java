99a98d99b4b5e93c117ac80f8c119a8874399d0b
Nigel <nigelmassey90@gmail.com>
2020-07-12 15:36:44 +0100

    Implemented controller tests using the @WebMvcTest annotation

--- src/test/java/com/example/WhoAmI/WhoAmIControllerTests.java
+++ src/test/java/com/example/WhoAmI/WhoAmIControllerTests.java
@@ -1,8 +1,51 @@
 package com.example.WhoAmI;
 
 import com.example.WhoAmI.controllers.WhoAmIController;
+import com.example.WhoAmI.models.WhoAmIRequestModel;
+import com.example.WhoAmI.services.AgifyService;
+import com.example.WhoAmI.services.GenderizeService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(value = WhoAmIController.class)
 public class WhoAmIControllerTests {
+
+    @Autowired
+    private MockMvc mockMvc;
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @MockBean
+    private AgifyService agifyService;
+    @MockBean
+    private GenderizeService genderizeService;
+
+    @Test
+    public void shouldReturnDefaultMessage() throws Exception {
+
+        String name = "Nigel";
+        String country_code = "GB";
+
+        when(agifyService.getAgeFromNameAndLocation(name, country_code)).thenReturn(12);
+        when(genderizeService.getGenderFromNameAndLocation(name, country_code)).thenReturn("male");
+
+        this.mockMvc.perform(post("/whoami")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(new WhoAmIRequestModel(name, country_code))))
+                .andDo(print()).andExpect(status().isOk());
+
+    }
+
 }
